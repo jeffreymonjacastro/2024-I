83a45b28996336363bdded7d0a52292c76a80762
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2024-04-01 08:58:47 -0500

    Clase 1

---
+++ DBP-TA/S1/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+  return {"Hello": "World"}
+
+@app.get("/hola")
+def getHola():
+  return {"message": "juas juas"}
