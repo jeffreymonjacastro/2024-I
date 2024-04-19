685796393f369f1cd370b37fff933ca712b354da
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2024-04-19 11:42:36 -0500

    Studying Spring Boot

--- DBP-TA/S2/HelloWorld/src/main/java/com/example/helloworld2/Producto/domain/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.helloworld2.Producto.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ProductoRepository extends JpaRepository<Producto, UUID> {
-}
